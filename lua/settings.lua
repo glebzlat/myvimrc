8bbe6e8e80e347e345d7d145907140ee87fae5cb
edKotinsky <edkotinsky@gmail.com>
2022-12-25 14:59:42 +0600

    Add mappings to cmp

--- lua/settings.lua
+++ lua/settings.lua
@@ -102,6 +102,19 @@ require 'Comment'.setup()
 local mason_root_dir = vim.fn.stdpath 'data' .. '/mason'
 local mason_bin_dir = mason_root_dir .. '/bin'
 
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0
+      and vim.api.nvim_buf_get_lines(
+        0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local feedkey = function(key, mode)
+  vim.api.nvim_feedkeys(
+    vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
 local cmp = require 'cmp'
 cmp.setup {
   snippet = {
@@ -124,6 +137,27 @@ cmp.setup {
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
     ['<Cr>'] = cmp.mapping.confirm({ select = true }),
+
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif vim.fn["vsnip#available"](1) == 1 then
+        feedkey("<Plug>(vsnip-expand-or-jump)", "")
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+        feedkey("<Plug>(vsnip-jump-prev)", "")
+      end
+    end, { "i", "s" }),
+
   }),
   formatting = {
     fields = {
@@ -135,7 +169,8 @@ cmp.setup {
       vim_item.menu = ({
         vsnip = '[Snippet]',
         buffer = '[Buffer]',
-        path = '[Path]'
+        path = '[Path]',
+        lsp = '[LSP]'
       })[entry.source.name]
       return vim_item
     end
