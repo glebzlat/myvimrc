95bb6a375e1d59f6c75de11d58d91dc7a8757193
edKotinsky <edkotinsky@gmail.com>
2023-05-09 19:13:34 +0600

    Remove useless boilerplate that doesn't work and just compile neoclip

--- lua/plugin/neoclip.lua
+++ lua/plugin/neoclip.lua
@@ -1,31 +1,14 @@
 return {
   "matveyt/neoclip",
   build = function()
-    local plugin_path = vim.fn.stdpath "data"
-      .. "/site/pack/packer/start/neoclip"
-    local plugin_src_path = plugin_path .. "/src"
+    local plugin_path = vim.fn.stdpath "data" .. "/lazy/neoclip"
+    local src_path = plugin_path .. "/src"
+    local build_path = src_path .. "/build"
+    local configure = "cmake -B " .. build_path .. " -S " .. src_path
+    local build = "cmake --build " .. build_path .. " --target install"
+    local cmd = configure .. " && " .. build
 
-    local function handler(_, data, event)
-      local loglevel = vim.log.levels.INFO
-      if event == "stderr" then
-        local msg = "Error compiling neoclip: " .. data .. "\n"
-        vim.api.nvim_notify(msg, loglevel, {})
-      elseif "stdout" then
-        local msg = "Neoclip compiled successfully: " .. data .. "\n"
-        vim.api.nvim_notify(msg, loglevel, {})
-      else
-        local msg = "Neoclip complilation process exited\n"
-        vim.api.nvim_notify(msg, loglevel, {})
-      end
-    end
-
-    vim.fn.jobstart(
-      "cmake -B build -S . && cmake --build build --target install",
-      {
-        cwd = plugin_src_path,
-        on_stdout = handler,
-        on_stderr = handler,
-      }
-    )
+    local output = vim.fn.system(cmd)
+    vim.api.nvim_notify(output, vim.log.levels.INFO, {})
   end,
 }
