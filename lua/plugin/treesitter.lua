95cf18a0e7a8c4211d7db3dfa7072379f803e0d4
Dave <dave@alpine.Home>
2023-06-10 12:13:09 +0600

    Add treeesitter-objects

--- lua/plugin/treesitter.lua
+++ lua/plugin/treesitter.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  "nvim-treesitter/nvim-treesitter-textobjects",
   build = function()
     require("nvim-treesitter.install").update { with_sync = true }
   end,
