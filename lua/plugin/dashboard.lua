082eed22a108891292e9a9efcbb11412a89452fe
glebzlat <edkotinsky@gmail.com>
2023-09-09 10:41:53 +0600

    Migrate to alpha-nvim

--- lua/plugin/dashboard.lua
+++ lua/plugin/dashboard.lua
@@ -1,32 +1,116 @@
 return {
-  "glepnir/dashboard-nvim",
-  commit = "5ccce7b50c8ce81c01956864ff51b455ee6933c4",
+  "goolord/alpha-nvim",
   config = function()
-    local db = require "dashboard"
-    db.custom_header = {
-      "███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗",
-      "████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║",
-      "██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║",
-      "██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║",
-      "██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║",
-      "╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝",
+    local header = {
+      type = "text",
+      val = {
+        "███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗",
+        "████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║",
+        "██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║",
+        "██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║",
+        "██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║",
+        "╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝",
+      },
+      opts = {
+        position = "center",
+        hl = "Type",
+      },
     }
-    db.custom_center = {
-      {
-        desc = "File browser",
-        action = "Telescope file_browser",
+
+    local function footer_val()
+      local lazy = require "lazy"
+      return {
+        string.format("Installed %d plugins", lazy.stats().count),
+      }
+    end
+
+    local footer = {
+      type = "text",
+      val = footer_val(),
+      opts = {
+        position = "center",
+        hl = "Number",
       },
-      {
-        desc = "Find file",
-        action = "Telescope find_files",
+    }
+
+    local function button(shortcut, val, keybind, keybind_opts)
+      local opts = {
+        position = "center",
+        shortcut = shortcut,
+        cursor = 3,
+        width = 50,
+        align_shortcut = "right",
+        hl_shortcut = "Keyword",
+      }
+
+      if keybind then
+        keybind_opts = vim.F.if_nil(
+          keybind_opts,
+          { noremap = true, silent = true, nowait = true }
+        )
+        opts.keymap = { "n", shortcut, keybind, keybind_opts }
+      end
+
+      local function on_press()
+        local key = vim.api.nvim_replace_termcodes(
+          keybind or shortcut .. "<Ignore>",
+          true,
+          false,
+          true
+        )
+        vim.api.nvim_feedkeys(key, "t", false)
+      end
+
+      return {
+        type = "button",
+        val = val,
+        on_press = on_press,
+        opts = opts,
+      }
+    end
+
+    local buttons = {
+      type = "group",
+      val = {
+        button("e", "New file", "<cmd>ene<CR>"),
+        button("<leader>tf", "Find file"),
+        button("<Space>", "File Browser"),
+        button("<leader>th", "Browse help"),
+        button("<leader>tk", "Keymaps"),
       },
-      {
-        desc = "Git status",
-        action = "Telescope git_status",
+      opts = {
+        spacing = 1,
       },
-      {
-        desc = "Open terminal",
-        action = "ToggleTerm",
+    }
+
+    local terminal = {
+      type = "terminal",
+      command = nil,
+      width = 69,
+      height = 8,
+      opts = {
+        redraw = true,
+        window_config = {},
+      },
+    }
+
+    local section = {
+      terminal = terminal,
+      header = header,
+      buttons = buttons,
+      footer = footer,
+    }
+
+    require("alpha").setup {
+      layout = {
+        { type = "padding", val = 2 },
+        section.header,
+        { type = "padding", val = 2 },
+        section.buttons,
+        section.footer,
+      },
+      opts = {
+        margin = 5,
       },
     }
   end,
