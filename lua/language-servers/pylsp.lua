47f7ccbeed43b7379d5c1002b9387c0d57d4b791
glebzlat <mrkotofey@inbox.ru>
2024-03-29 21:38:52 +0500

    Experimental. Hardcode a path to system-wide Python site-packages for jedi to provide autocompletion for modules installed here.

--- lua/language-servers/pylsp.lua
+++ lua/language-servers/pylsp.lua
@@ -11,6 +11,16 @@ return {
           },
           maxLineLength = 80,
         },
+        -- EXPERIMENTAL
+        -- This path is valid for Fedora Workstation 39. On other systems
+        -- it should be different. I think, if this cause no issues, I will
+        -- write a utility which determines this path automatically.
+        -- But for now it is hardcoded here.
+        jedi = {
+          extra_paths = {
+            "/usr/lib64/python3.12/site-packages"
+          }
+        }
       },
     },
   },
