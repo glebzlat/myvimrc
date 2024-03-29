271a6c2255ae7b988c96c9f9b3e55ff0f2120891
glebzlat <mrkotofey@inbox.ru>
2024-03-29 21:31:50 +0500

    Update lspconfig setup to the Suggested configuration

--- lua/plugin/lsp.lua
+++ lua/plugin/lsp.lua
@@ -17,32 +17,6 @@ return {
       { noremap = true, silent = true }
     )
 
-    local on_attach = function(_, bufnr)
-      local bufopts = { noremap = true, silent = true, buffer = bufnr }
-      map("n", "<leader><leader>ca", vim.lsp.buf.code_action, bufopts)
-      map("n", "<leader>gD", vim.lsp.buf.declaration, bufopts)
-      map("n", "<leader>gd", vim.lsp.buf.definition, bufopts)
-      map("n", "<leader>K", vim.lsp.buf.hover, bufopts)
-      map("n", "<leader>gi", vim.lsp.buf.implementation, bufopts)
-      map("n", "<leader>tD", vim.lsp.buf.type_definition, bufopts)
-      map("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
-      map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, bufopts)
-      map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
-      map(
-        "n",
-        "<leader>wl",
-        function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end,
-        bufopts
-      )
-      map("n", "<leader><C-k>", vim.lsp.buf.signature_help, bufopts)
-      map(
-        "n",
-        "<leader>bf",
-        function() vim.lsp.buf.format { async = true } end,
-        bufopts
-      )
-    end
-
     local lspconfig = require "lspconfig"
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
     local lsp_flags = { debounce_text_changes = 150 }
@@ -55,12 +29,10 @@ return {
     -- for each file in language_servers_dir:
     --   load configuration and setup the server
     for filename in vim.fs.dir(language_servers_dir) do
-      local module = language_servers .. "." .. fnamemodify(filename, ':r')
+      local module = language_servers .. "." .. fnamemodify(filename, ":r")
       local server = require(module)
 
-      -- apply default `on_attach`, `capabilities` and `flags` if custom
-      -- not provided
-      if not server["on_attach"] then server["on_attach"] = on_attach end
+      -- apply default `capabilities` and `flags` if custom are not provided
       if not server["capabilities"] then
         server["capabilities"] = capabilities
       end
@@ -70,5 +42,46 @@ return {
       if not config["document_config"] then return end
       config.setup(server)
     end
+
+    -- Global mappings.
+    -- See `:help vim.diagnostic.*` for documentation on any of the below
+    -- functions
+    map("n", "<leader>e", vim.diagnostic.open_float)
+    map("n", "[d", vim.diagnostic.goto_prev)
+    map("n", "]d", vim.diagnostic.goto_next)
+    map("n", "<leader>q", vim.diagnostic.setloclist)
+
+    -- Use LspAttach autocommand to only map the following keys
+    -- after the language server attaches to the current buffer
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+      callback = function(ev)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+        -- Buffer local mappings
+        local bufopts = { noremap = true, silent = true, buffer = ev.buf }
+        map("n", "<leader>ac", vim.lsp.buf.code_action, bufopts)
+        map("n", "<leader>gD", vim.lsp.buf.declaration, bufopts)
+        map("n", "<leader>gd", vim.lsp.buf.definition, bufopts)
+        map("n", "<leader>K", vim.lsp.buf.hover, bufopts)
+        map("n", "<leader>gi", vim.lsp.buf.implementation, bufopts)
+        map("n", "<leader>tD", vim.lsp.buf.type_definition, bufopts)
+        map("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
+        map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+        map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+        map("n", "<leader><C-k>", vim.lsp.buf.signature_help, bufopts)
+        -- stylua: ignore start
+        map("n", "<leader>wl",
+          function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+          end, bufopts)
+        map("n", "<leader>bf",
+          function()
+            vim.lsp.buf.format { async = true }
+          end, bufopts)
+        -- stylua: ignore end
+      end,
+    })
   end,
 }
