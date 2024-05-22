f44166e9602c7ea6879fdc93633be277461b9abb
glebzlat <mrkotofey@inbox.ru>
2024-05-22 16:48:58 +0500

    Change emmet_ls to emmet_language_server

--- lua/language-servers/emmet.lua
+++ lua/language-servers/emmet.lua
@@ -2,30 +2,13 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 return {
-  "emmet_ls",
+  "emmet_language_server",
   capabilities = capabilities,
-  filetypes = {
-    "astro",
-    "css",
-    "eruby",
-    "html",
-    "htmldjango",
-    "javascriptreact",
-    "js",
-    "less",
-    "pug",
-    "sass",
-    "scss",
-    "svelte",
-    "typescriptreact",
-    "vue",
-  },
-  init_options = {
-    html = {
-      options = {
-        ["bem.enabled"] = true,
-      },
-    },
-  },
-  cmd = { "emmet-ls", "--stdio" },
+  -- init_options = {
+  --   html = {
+  --     options = {
+  --       ["bem.enabled"] = true,
+  --     },
+  --   },
+  -- },
 }
