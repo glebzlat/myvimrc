06fe7aa8b5f2107f84f4c0da7b14077c4ed62720
Gleb Zlatanov <g.zlatanov@dss.kz>
2025-07-10 15:33:05 +0500

    Replace simrat39/symbols-outline.nvim by hedyhli/outline.nvim

--- lua/plugin/symbol-outline.lua
+++ lua/plugin/symbol-outline.lua
@@ -1,52 +1,37 @@
 return {
-  "simrat39/symbols-outline.nvim",
-  config = function()
-    local options = {
-      symbols = {
-        keymaps = {
-          close = { "q" },
-          goto_location = "<Cr>",
-          focus_location = "o",
-          hover_symbol = "<C-space>",
-          toggle_preview = "K",
-          rename_symbol = "r",
-          code_actions = "a",
-          fold = "h",
-          unfold = "l",
-          fold_all = "W",
-          unfold_all = "E",
-          fold_reset = "R",
-        },
-        File = { icon = "file", hl = "@text.uri" },
-        Module = { icon = "module", hl = "@namespace" },
-        Namespace = { icon = "namespace", hl = "@namespace" },
-        Package = { icon = "pack", hl = "@namespace" },
-        Class = { icon = "class", hl = "@type" },
-        Method = { icon = "method", hl = "@method" },
-        Property = { icon = "prop", hl = "@method" },
-        Field = { icon = "field", hl = "@field" },
-        Constructor = { icon = "ctor", hl = "@constructor" },
-        Enum = { icon = "enum", hl = "@type" },
-        Interface = { icon = "iface", hl = "@type" },
-        Function = { icon = "func", hl = "@function" },
-        Variable = { icon = "var", hl = "@constant" },
-        Constant = { icon = "const", hl = "@constant" },
-        String = { icon = "str", hl = "@string" },
-        Number = { icon = "#", hl = "@number" },
-        Boolean = { icon = "bool", hl = "@boolean" },
-        Array = { icon = "arr", hl = "@constant" },
-        Object = { icon = "obj", hl = "@type" },
-        Key = { icon = "key", hl = "@type" },
-        Null = { icon = "NULL", hl = "@type" },
-        EnumMember = { icon = "enumm", hl = "@field" },
-        Struct = { icon = "struct", hl = "@type" },
-        Event = { icon = "event", hl = "@type" },
-        Operator = { icon = "+", hl = "@operator" },
-        TypeParameter = { icon = "𝙏", hl = "@parameter" },
-        Component = { icon = "comp", hl = "@function" },
-        Fragment = { icon = "frag", hl = "@constant" },
+  "hedyhli/outline.nvim",
+  lazy = true,
+  cmd = { "Outline", "OutlineOpen" },
+  keys = {
+    { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+  },
+  opts = {
+    symbols = {
+      outline_items = {
+        show_symbol_lineno = true,
+        auto_set_cursor = false,
       },
-    }
-    require("symbols-outline").setup(options)
-  end,
+      filter = {
+        python = { "Function", "Class", "Method", "Constructor" },
+      },
+      icons = {
+        -- Use Unicode symbols to take into account environments with no
+        -- custom font.
+        File = { icon = "F" },
+        Module = { icon = "M" },
+        Namespace = { icon = "ℕ" },
+        Package = { icon = "P" },
+        Property = { icon = "ⅈ" },
+        Field = { icon = "ⅈ" },
+        Constructor = { icon = "ℭ" },
+        Interface = { icon = "ℐ" },
+        Function = { icon = "ℱ" },
+        Variable = { icon = "v" },
+        Constant = { icon = "ℼ" },
+        StaticMethod = { icon = "ƒ" },
+        EnumMember = { icon = "e" },
+        Parameter = { icon = "℘" },
+      },
+    },
+  },
 }
