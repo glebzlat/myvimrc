fac54a6f652af499b3be97a030928555d7e77773
Gleb Zlatanov <mrkotofey@inbox.ru>
2025-08-07 22:58:54 +0500

    Disable LSP auto-enable

--- lua/plugin/mason-lspconfig.lua
+++ lua/plugin/mason-lspconfig.lua
@@ -1,8 +1,10 @@
 return {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {},
-    dependencies = {
-        { "mason-org/mason.nvim", opts = {} },
-        "neovim/nvim-lspconfig",
-    },
+  "mason-org/mason-lspconfig.nvim",
+  opts = {
+    automatic_enable = false,
+  },
+  dependencies = {
+    { "mason-org/mason.nvim", opts = {} },
+    "neovim/nvim-lspconfig",
+  },
 }
